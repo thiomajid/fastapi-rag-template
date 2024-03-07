d54ffb4bb1e73ac2b6a9802e892924fa24880b1d
thiomajid <82466066+ATOM36@users.noreply.github.com>
2024-03-07 14:26:13 +0100

    Exposing the demo with 0.0.0.0 as host

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
-# from app.embeddings import create_embeddings
-# from app.query_engine import model_pipeline
+from app.embeddings import create_embeddings
+from app.query_engine import model_pipeline
 
 with gr.Blocks() as demo:
     gr.Markdown("# Clarins chatbot demo")
@@ -13,12 +13,16 @@ with gr.Blocks() as demo:
         with gr.Column():
             output = gr.Textbox(label="Output", lines=10)
 
-    # event = run_query_btn.click(
-    #     model_pipeline,
-    #     inputs=[question],
-    #     outputs=[output],
-    # )
-    # embedding_btn.click(create_embeddings)
+    event = run_query_btn.click(
+        model_pipeline,
+        inputs=[question],
+        outputs=[output],
+    )
+    embedding_btn.click(create_embeddings)
 
 
-demo.launch(server_port=8000)
+demo.launch(
+    server_name="0.0.0.0",
+    server_port=8000,
+    share=True,
+)
